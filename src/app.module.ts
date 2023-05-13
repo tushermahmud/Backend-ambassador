b112504e56dc6c3e8ca21a6f4bbb6d9edfcf1460
tushermahmud <sazzadmahmud01795355849@gmail.com>
2023-05-14 02:09:11 +0600

    product crud done

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { Product } from './product/product.entity';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -17,11 +20,13 @@ import { AuthModule } from './auth/auth.module';
       username: 'emp',
       password: 'tiptop123',
       database: 'ambassador_db',
-      entities: [User],
+      entities: [User, Product],
       synchronize: true,
     }),
     UserModule,
     AuthModule,
+    ProductModule,
+    OrderModule,
   ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from '../shared/abstract.service';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
-export class UserService {
-    constructor(@InjectRepository(User) private readonly userRepository:Repository<User>){}
-    async save(options):Promise<User>{
-        return await this.userRepository.save(options)
-    }
-
-    async findOne(email):Promise<User>{
-        return await this.userRepository.findOne({where:{email}});
-    }
-
-    async update(id:number, data){
-        return await this.userRepository.update(id, data); 
-    }
-
-    async find(options):Promise<User[]>{
-        return await this.userRepository.find({where: options});
-    }
-
-    async findById(id){
-        return await this.userRepository.findOne({where:{id}})
+export class UserService extends AbstractService{
+    constructor(@InjectRepository(User) private readonly userRepository:Repository<User>){
+        super(userRepository)
     }
 }
